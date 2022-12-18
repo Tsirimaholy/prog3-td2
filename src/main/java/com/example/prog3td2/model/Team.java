46d2cfeb94ba5d406289256e7b922ec2b569ff9e
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-12-18 17:59:50 +0300

    feat: adding team entity

---
+++ src/main/java/com/example/prog3td2/model/Team.java
@@ -0,0 +1,32 @@
+package com.example.prog3td2.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "team")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Team {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @ManyToMany(mappedBy = "teams")
+    private List<Sponsor> sponsors;
+}
