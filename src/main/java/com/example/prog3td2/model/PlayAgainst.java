98001e45ab56f45d929a2a0b2653e9b5c40c12d4
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-12-18 18:00:55 +0300

    feat: adding playAgainst entity

---
+++ src/main/java/com/example/prog3td2/model/PlayAgainst.java
@@ -0,0 +1,44 @@
+package com.example.prog3td2.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "play_against")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlayAgainst {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team1_id")
+    private Team team1;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team2_id")
+    private Team team2;
+
+    @Column(name = "stadium", length = 100)
+    private String stadium;
+
+    @Column(name = "date_time")
+    private Instant dateTime;
+}
