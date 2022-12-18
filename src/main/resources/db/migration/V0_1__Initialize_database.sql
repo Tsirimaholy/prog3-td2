51c032b3bfef2bd8be784d4f8cf96dd1e3f7670d
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-12-18 17:56:27 +0300

    feat: adding database initializer migration

---
+++ src/main/resources/db/migration/V0_1__Initialize_database.sql
@@ -0,0 +1,39 @@
+CREATE TABLE "team"
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE "player"
+(
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(255),
+    number  INTEGER,
+    team_id INTEGER REFERENCES team (id)
+);
+
+CREATE TABLE "sponsor"
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE "team_sponsor"
+(
+    id         SERIAL PRIMARY KEY,
+    team_id    INTEGER REFERENCES team (id),
+    sponsor_id INTEGER REFERENCES sponsor (id),
+    CONSTRAINT unique__team_sponsor UNIQUE (sponsor_id, team_id)
+);
+
+
+CREATE TABLE "play_against"
+(
+    id SERIAL PRIMARY KEY,
+    team1_id INTEGER REFERENCES team (id),
+    team2_id INTEGER REFERENCES team (id),
+    stadium VARCHAR(100),
+    date_time timestamp,
+    CONSTRAINT team_are_different CHECK ( team2_id != team2_id ),
+    UNIQUE (team1_id, team2_id)
+);
