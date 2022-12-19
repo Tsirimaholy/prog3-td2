61784eeca0d93e1abafdc69a24157025242c752c
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-12-19 21:22:20 +0300

    fix: typo

--- src/main/java/com/example/prog3td2/model/Team.java
+++ src/main/java/com/example/prog3td2/model/Team.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
